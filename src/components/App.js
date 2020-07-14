fa104c320066237f6285d395d1cd5739a4703c70
Andry <randrianirina.andry@protonmail.com>
2020-07-14 15:40:42 +0200

    Initialize project

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => (
+    <React.Fragment>
+        <h2 className="ui center aligned header">Calcul salaire</h2>
+        <div className="ui stackable celled grid container">
+            <div className="two column row">
+                <div className="column">
+                    <div className="ui segment">Formulaire 1</div>
+                </div>
+                <div className="column">
+                    <div className="ui segment">Formulaire 2</div>
+                </div>
+            </div>
+        </div>
+    </React.Fragment>
+);
+
+export default App;
