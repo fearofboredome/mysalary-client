e418dd39cec53638d933b460fc712078c8c567c0
Andry <randrianirina.andry@protonmail.com>
2020-07-18 22:32:25 +0200

    Initialize form

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,64 @@
 import React from 'react';
 
 const App = () => (
-    <React.Fragment>
-        <h2 className="ui center aligned header">Calcul salaire</h2>
-        <div className="ui stackable celled grid container">
-            <div className="two column row">
-                <div className="column">
-                    <div className="ui segment">Formulaire 1</div>
-                </div>
-                <div className="column">
-                    <div className="ui segment">Formulaire 2</div>
-                </div>
+  <React.Fragment>
+    <h2 className="ui center aligned header">Calcul salaire</h2>
+    <form className="ui form">
+      <div className="ui stackable celled grid container">
+        <div className="two column row">
+          <div className="column">
+            <div className="two fields">
+              <div className="field">
+                <label>Horaire brut</label>
+                <input type="text" placeholder="0"/>
+              </div>
+              <div className="field">
+                <label>Horaire net</label>
+                <input type="text" placeholder="0"/>
+              </div>
             </div>
+            <div className="two fields">
+              <div className="field">
+                <label>Mensuel brut</label>
+                <input type="text" placeholder="0"/>
+              </div>
+              <div className="field">
+                <label>Mensuel net</label>
+                <input type="text" placeholder="0"/>
+              </div>
+            </div>
+            <div className="two fields">
+              <div className="field">
+                <label>Annuel brut</label>
+                <input type="text" placeholder="0"/>
+              </div>
+              <div className="field">
+                <label>Annuel net</label>
+                <input type="text" placeholder="0"/>
+              </div>
+            </div>
+            <div className="inline fields">
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input type="radio" name="salarie_non_cadre" checked="" tabIndex="0" className="hidden"/>
+                  <label>Salarié non cadre</label>
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input type="radio" name="salarie_cadre" checked="" tabIndex="0" className="hidden"/>
+                  <label>Salarié cadre</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="ui segment">Formulaire 2</div>
+          </div>
         </div>
-    </React.Fragment>
+      </div>
+    </form>
+  </React.Fragment>
 );
 
 export default App;
